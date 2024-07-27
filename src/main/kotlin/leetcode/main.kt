52b2165ea2d95f85170d4e39b7b0ba3d1fcd3f93
jatin <jatinsinghal404@gmail.com>
2024-07-27 12:57:12 +0530

    FIX main branch

--- src/main/kotlin/leetcode/main.kt
+++ src/main/kotlin/leetcode/main.kt
@@ -1,11 +1,6 @@
 package leetcode
 
-import Solution
 
 fun main() {
-    val sol = Solution()
-    val ans = sol.runningSum(intArrayOf(1,2,3,4))
-    ans.forEach {
-        print("$it ")
-    }
+
 }
